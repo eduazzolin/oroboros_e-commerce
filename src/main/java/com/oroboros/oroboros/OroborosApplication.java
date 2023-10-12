5e25297aa88e4904776c63ee1d7fddf7e2458682
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2023-10-12 14:20:01 -0300

    Conversão para java spring

---
+++ src/main/java/com/oroboros/oroboros/OroborosApplication.java
@@ -0,0 +1,13 @@
+package com.oroboros.oroboros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OroborosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OroborosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oroboros/oroboros/OroborosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oroboros.oroboros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OroborosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
